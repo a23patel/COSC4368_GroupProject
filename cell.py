2a47b00e52a394cb73661b3a313d4675624f31eb
a23patel <abraarpatel23@gmail.com>
2023-03-11 02:40:18 -0600

    made some minor changes and added the cost function

--- cell.py
+++ cell.py
@@ -1,50 +1,63 @@
 import numpy as np
 
-class Cell:
+class Cells:
     def __init__(self):
         self.type = 'Basic'
         self.occupancy = []
-        self.numBlocks = 0
-        # cost? = -1 for Basic & = -2 for Risk cells
+        self.num_blocks = 0
+        self.cost = -1   # default cost in traversing blocks to a particular cell is -1 for Basic cells 
     
     # type methods
-    def getType(self): # Basic, Pickup, Dropoff, Risk
+    def getType(self):   # Basic, Pickup, Dropoff, Risk
         return self.type
     def setType(self, _type):
         if _type == 'Pickup':
             self.numBlocks = 10
+        if _type == 'Basic':
+            self.cost = -1
+        elif _type == 'Risk':
+            self.cost = -2
         self.type = _type
     
     # agent methods
-    def occupied(self):
-        return self.occupancy # returns false if occupancy list is empty
+    def is_occupied(self):
+        return bool(self.occupancy)  # returns false if occupancy list is empty
     def whichAgent(self):
-        if self.occupied():
+        if self.is_occupied():
             return self.occupancy[0]
     def addAgent(self, _agent):
-        if not(self.occupied()):
+        if not(self.is_occupied()):
             self.occupancy.append(_agent)
-    def rmAgent(self, _agent):
-        if self.occupied():
+    def removeAgent(self, _agent):
+        if self.is_occupied():
             self.occupancy.remove(_agent)
 
     # block methods
     def getNumBlocks(self):
-        return self.numBlocks
+        return self.num_blocks
     def addBlock(self):
-        if self.type == 'Dropoff' and self.numBlocks < 5: # capacity is 5 blocks
+        if self.type == 'Dropoff' and self.num_blocks < 5:   # capacity is 5 blocks
             self.numBlocks += 1
-    def rmBlock(self):
+    def removeBlock(self):
         if self.numBlocks > 0:
             self.numBlocks -= 1
+    def getCost(self):
+        if self.is_occupied():
+            return None
+        elif self.type == 'Basic' or self.type == 'Risk':
+            return self.cost
+        else:
+            return self.num_blocks    
 
-StateSpace = np.empty(shape=(3,3,3), dtype=object, order='C') # 'C' means row-major order in memory
+
+#creating the state space
+StateSpace = np.empty(shape=(3,3,3), dtype=object, order='C')    # 'C' means row-major order in memory
 
 # initialize StateSpace as all basic cells
 for x in range(StateSpace.shape[0]):
     for y in range(StateSpace.shape[1]):
         for z in range(StateSpace.shape[2]):
-            StateSpace[x,y,z] = Cell()
+            StateSpace[x,y,z] = Cells()
 
 # update cells according to http://www2.cs.uh.edu/~ceick/ai/2023-World.pptx
 
@@ -62,11 +75,12 @@ StateSpace[0,0,2].setType('Dropoff')
 StateSpace[2,0,0].setType('Dropoff')
 StateSpace[2,1,2].setType('Dropoff')
 
-# visualize SS to compare to assignment description
-for z in range(StateSpace.shape[0]):
+# visualizing state space to compare to project description
+for x in range(StateSpace.shape[0]):
     for y in range(StateSpace.shape[1]):
-        for x in range(StateSpace.shape[2]):
+        for z in range(StateSpace.shape[2]):
             cell = StateSpace[x,y,z]
-            print(f"location: ({x+1},{y+1},{z+1})\n\ttype: {cell.getType()}\n" +
-                  f"\tnumber of blocks: {cell.getNumBlocks()}\n" +
-                  f"\twhich agent: {cell.whichAgent()}\n")
+            print(f"Location of blocks: ({x+1},{y+1},{z+1})\n\tType of Block: {cell.getType()}\n" +
+                  f"\tNumber of blocks: {cell.getNumBlocks()}\n" +
+                  f"\tAgent: {cell.whichAgent()}\n" +
+                  f"\tCost of traversing blocks: {cell.getCost()}\n")
